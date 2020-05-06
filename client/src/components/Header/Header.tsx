d3541531814300785e3c2a423f01b2c6394656dc
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-06 20:15:21 -0300

    :zap: Added component to handle w/ consultant componet

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,50 +1,22 @@
 import React, { ReactElement } from "react";
-import "./Header.scss";
-import {
-  LogoutOutlined,
-  UserAddOutlined,
-  UsergroupAddOutlined,
-} from "@ant-design/icons";
+import { LogoutOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
-import { Input, Tooltip } from "antd";
+import { ConsultantButtons } from "../index";
+import "./Header.scss";
 
 interface HeaderProps {
   name: string;
+  consultant: Boolean;
 }
 
-export function Header({ name }: HeaderProps): ReactElement {
-  const { Search } = Input;
+export function Header({ name, consultant }: HeaderProps): ReactElement {
   return (
     <div className="header">
       <Link to="/" className="header__button">
         <LogoutOutlined />
       </Link>
       Welcome, &nbsp;<span className="--bold"> {name}!</span>
-      <section className="header__consultant">
-        <Tooltip
-          trigger={["focus"]}
-          title="Insert a username or a date"
-          placement="topLeft"
-        >
-          <Search
-            placeholder="Search..."
-            maxLength={25}
-            className="consultant__search"
-          />
-        </Tooltip>
-        <section className="consultant__buttons">
-          <button className="consultant__button">
-            <Tooltip title="Generate users">
-              <UserAddOutlined />
-            </Tooltip>
-          </button>
-          <button className="consultant__button">
-            <Tooltip title="Generate groups">
-              <UsergroupAddOutlined />
-            </Tooltip>
-          </button>
-        </section>
-      </section>
+      {!consultant ? null : <ConsultantButtons />}
     </div>
   );
 }
