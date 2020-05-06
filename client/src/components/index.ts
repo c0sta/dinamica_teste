86091f6a611b9b8c0c1520d0504710f640608f40
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-06 20:14:13 -0300

    :zap: Added new component to export file

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,3 +1,4 @@
 export { Header } from "./Header/Header";
 export { Chat } from "./Chat/Chat";
 export { Users } from "./Users/Users";
+export { ConsultantButtons } from "./ConsultantButtons/ConsultantButtons";
