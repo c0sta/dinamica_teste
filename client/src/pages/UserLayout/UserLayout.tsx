b4aa132cb2ce9843032242b925c0d34f303d95d4
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-06 19:57:42 -0300

    :sparkles: Done user basic interface

--- client/src/pages/UserLayout/UserLayout.tsx
+++ client/src/pages/UserLayout/UserLayout.tsx
@@ -1,5 +1,17 @@
 import React, { ReactElement } from "react";
+import { useParams } from "react-router-dom";
+import { Chat, Users, Header } from "../../components/index";
+
+import "./UserLayout.scss";
 
 export function UserLayout(): ReactElement {
-  return <div>UserLayout</div>;
+  let { name } = useParams();
+  // const username = useSelector((state: RootState) => state.session.username);
+  return (
+    <div className="user-page">
+      <Header name={name} />
+      <Users />
+      <Chat />
+    </div>
+  );
 }
