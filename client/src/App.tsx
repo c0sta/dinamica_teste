d8a5c8c5b29f332821d43d376869162ecb1fb61f
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-06 20:13:19 -0300

    :art: Removed createStore logic from App
    Moved to the store/index.ts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Routes from "./routes/routes";
-import { rootReducer } from "./redux/store/index";
+import { store } from "./redux/store/index";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
-
+import "./styles/_global.scss";
 const App: React.FC = () => {
-  const store = createStore(rootReducer);
   return (
     <Provider store={store}>
       <Routes />
