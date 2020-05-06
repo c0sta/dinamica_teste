488a2b18c93c66b1c54a3e06de6fcaaf6a25a0c6
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-06 00:20:44 -0300

    :sparkles: chat redux types and action types

---
+++ client/src/redux/store/chat/chat-types.ts
@@ -0,0 +1,26 @@
+export const SEND_MESSAGE = "SEND_MESSAGE";
+export const DELETE_MESSAGE = "DELETE_MESSAGE";
+
+export interface Message {
+  user: string;
+  message: string;
+  timestamp: Date;
+}
+
+export interface ChatState {
+  messages: Message[];
+}
+
+interface SendMessageAction {
+  type: typeof SEND_MESSAGE;
+  payload: Message;
+}
+
+interface DeleteMessageAction {
+  type: typeof DELETE_MESSAGE;
+  meta: {
+    timestamp: Date;
+  };
+}
+
+export type ChatActionsTypes = SendMessageAction | DeleteMessageAction;
