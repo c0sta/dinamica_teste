bcbec5b3a746bdcbad6eb1a75860414b7316beb8
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-07 02:44:26 -0300

    :zap: Added Message componente to export file

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -2,3 +2,4 @@ export { Header } from "./Header/Header";
 export { Chat } from "./Chat/Chat";
 export { Users } from "./Users/Users";
 export { ConsultantButtons } from "./ConsultantButtons/ConsultantButtons";
+export { Message } from "./Message/Message";
