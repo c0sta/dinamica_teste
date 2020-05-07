57e3cfbf504afdb0e311b03ff6ae7e9943601f02
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-07 02:45:38 -0300

    :sparkles: Added redux to chat component

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -1,26 +1,53 @@
 import React, { ReactElement } from "react";
 import { SendOutlined } from "@ant-design/icons";
+import { Spin } from "antd";
+import { formatDateNameAndHour } from "../../utils/format-helper";
+import { Message } from "../Message/Message";
 import "./Chat.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { SEND_MESSAGE } from "../../redux/store/chat/chat-types";
+import { RootState } from "../../redux/store";
 
-export function Chat(): ReactElement {
-  const messages = ["sadfas", "sadfas", "sadfas", "sadfas", "sadfas"];
-  // const [message, setMessase] = React.useState("");
+interface ChatProps {
+  name: string;
+}
 
-  // const handleSendMessage = () => {};
+export function Chat({ name }: ChatProps): ReactElement {
+  const [message, setMessage] = React.useState("");
+  const messages = useSelector((state: RootState) => state.chat.messages);
+  const dispatch = useDispatch();
+  const messageFormated: string = formatDateNameAndHour(name);
+
+  const handleSendMessage = () => {
+    dispatch({
+      type: SEND_MESSAGE,
+      payload: { user: name, message, timestamp: messageFormated },
+    });
+  };
 
   return (
     <div className="chat-container">
       <section className="chat-container__messages">
-        {messages.map((msg) => (
-          <h3>{msg}</h3>
-        ))}
+        {messages.length !== 0 ? (
+          messages.map((msg, index) => (
+            <Message
+              key={index}
+              message={msg.message}
+              timestamp={msg.timestamp}
+            />
+          ))
+        ) : (
+          <h1 className="messages__no-message">No messages yet...</h1>
+        )}
       </section>
+
       <section className="chat-container__input-container">
         <input
           className="input-container__input"
           placeholder="Insert message..."
+          onChange={(e) => setMessage(e.target.value)}
         />
-        <button className="input-container__button">
+        <button className="input-container__button" onClick={handleSendMessage}>
           <SendOutlined />
         </button>
       </section>
