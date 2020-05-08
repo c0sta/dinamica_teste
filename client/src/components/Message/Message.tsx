b3d863f633302f15994bf2080ddfa695a0e82cac
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-08 13:28:02 -0300

    :art: Added popover

--- client/src/components/Message/Message.tsx
+++ client/src/components/Message/Message.tsx
@@ -1,17 +1,50 @@
 import React, { ReactElement } from "react";
+import { Popover, Button } from "antd";
+import { DeleteOutlined } from "@ant-design/icons";
+import { useDispatch } from "react-redux";
 
 import "./Message.scss";
 
-interface MessageState {
+interface MessageProps {
+  handleDelete: () => void;
   timestamp: string;
   message: string;
+  consultant: Boolean;
 }
 
-export function Message({ message, timestamp }: MessageState): ReactElement {
-  return (
-    <div className="message-container">
+export function Message({
+  handleDelete,
+  message,
+  timestamp,
+  consultant,
+}: MessageProps): ReactElement {
+  const [visible, setVisible] = React.useState(false);
+
+  const MessageComponent = () => (
+    <button onFocus={() => setVisible(!visible)} className="message-container">
       <span className="message-container__timestamp">{timestamp}</span>
       <p className="message-container__message">{message}</p>
-    </div>
+    </button>
+  );
+  const PopoverContent = () => (
+    <Button onClick={handleDelete} type="dashed" danger>
+      Delete message &nbsp; <DeleteOutlined />
+    </Button>
+  );
+  return (
+    <>
+      {!consultant ? (
+        <MessageComponent />
+      ) : (
+        <Popover
+          trigger="hover"
+          visible={visible}
+          onVisibleChange={(visibility) => setVisible(visibility)}
+          content={PopoverContent}
+        >
+          <MessageComponent />
+        </Popover>
+      )}
+    </>
   );
 }
