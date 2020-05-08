392f3cc552a4589bacef5d1a64c241a8bc49724a
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-08 13:21:04 -0300

    :art: Added group context stuff

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useReducer } from "react";
 import Routes from "./routes/routes";
 import { store } from "./redux/store/index";
 import { Provider } from "react-redux";
 import "./styles/_global.scss";
+import { UserContext } from "./context/UserContext";
+import { GroupContext } from "./context/GroupContext";
+import {
+  usersReducer,
+  initialState as userInitialState,
+} from "./context/UserReducer";
+import {
+  groupsReducer,
+  initialState as groupsInitialState,
+} from "./context/GroupReducer";
+
 const App: React.FC = () => {
+  const [usersState, usersDispatch] = useReducer(
+    usersReducer,
+    userInitialState
+  );
+  const [groupsState, groupsDispatch] = useReducer<any>(
+    groupsReducer,
+    groupsInitialState
+  );
   return (
     <Provider store={store}>
-      <Routes />
+      <GroupContext.Provider value={{ groupsState, groupsDispatch }}>
+        <UserContext.Provider value={{ usersState, usersDispatch }}>
+          <Routes />
+        </UserContext.Provider>
+      </GroupContext.Provider>
     </Provider>
   );
 };
