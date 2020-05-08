918ea304325f4bdc753a63bf710467af56d3ffc9
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-08 13:21:46 -0300

    :art: Added delete func to chat

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -4,43 +4,88 @@ import { formatDateNameAndHour } from "../../utils/format-helper";
 import { Message } from "../Message/Message";
 import "./Chat.scss";
 import { useDispatch, useSelector } from "react-redux";
-import { SEND_MESSAGE } from "../../redux/store/chat/chat-types";
+import {
+  SEND_MESSAGE,
+  DELETE_MESSAGE,
+} from "../../redux/store/chat/chat-types";
 import { RootState } from "../../redux/store";
+import { Message as Msg } from "../../redux/store/chat/chat-types";
+import uuid from "../../utils/id-generator";
 import io from "socket.io-client";
 
 interface ChatProps {
   name: string;
+  consultant: Boolean;
 }
 
-export function Chat({ name }: ChatProps): ReactElement {
+export function Chat({ name, consultant }: ChatProps): ReactElement {
   const dispatch = useDispatch();
   const [message, setMessage] = React.useState("");
+  // const [messages, setMessages] = React.useState<Msg[]>([]);
   // Gets the messages from Redux store
   const messages = useSelector((state: RootState) => state.chat.messages);
   // Helper function that build s the Timestamp in the required format
   // 'DD/MM/YY - ‘username’ - HH:MM => ‘mensagem’
   const timestamp: string = formatDateNameAndHour(name);
 
-  const handleSendMessage = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    io("http://localhost:8080");
+  // Connects to socket
+  const socket = io("http://localhost:8080");
+
+  const updateMsgs = (newMsg: Msg) => {
     dispatch({
       type: SEND_MESSAGE,
-      payload: { user: name, message, timestamp },
+      payload: newMsg,
     });
-    // cleans input fieeld
+  };
+
+  React.useEffect(() => {
+    socket.emit("retrieveMessages");
+    socket.on("retrieveMessages", (messages: Msg[]) =>
+      dispatch({ type: SEND_MESSAGE, payload: [...messages] })
+    );
+  }, []);
+
+  React.useEffect(() => {
+    socket.on("receivedMessage", (newMessage: Msg) => updateMsgs(newMessage));
+  }, [messages]);
+
+  const handleSendMessage = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // Creates msgObject
+    const msgObject = {
+      id: uuid(),
+      user: name,
+      message,
+      timestamp,
+    };
+
+    if (message.length) {
+      socket.emit("sendMessage", msgObject);
+      // Sends msg structure to redux store
+      dispatch({
+        type: SEND_MESSAGE,
+        payload: msgObject,
+      });
+    }
+    // cleans input field
     setMessage("");
+  };
+  const handleDelete = (id: number): void => {
+    dispatch({ type: DELETE_MESSAGE, payload: id.toString() });
   };
 
   return (
     <div className="chat-container">
       <section className="chat-container__messages">
         {messages.length !== 0 ? (
-          messages.map((msg, index) => (
+          messages.map((msg: Msg) => (
             <Message
-              key={index}
+              key={msg.id}
+              handleDelete={() => handleDelete(msg.id)}
               message={msg.message}
               timestamp={msg.timestamp}
+              consultant={consultant}
             />
           ))
         ) : (
@@ -57,6 +102,7 @@ export function Chat({ name }: ChatProps): ReactElement {
           placeholder="Insert message..."
           onChange={(e) => setMessage(e.target.value)}
           value={message}
+          required
         />
         <button type="submit" className="input-container__button">
           <SendOutlined />
