0c4899119b113e8e21b1d2cd921e092174f3c465
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-08 13:32:09 -0300

    :fire: Removed session reducer

--- client/src/redux/store/index.ts
+++ client/src/redux/store/index.ts
@@ -1,10 +1,8 @@
 import { combineReducers, createStore, applyMiddleware } from "redux";
-import { sessionReducer } from "./session/session-reducer";
 import { chatReducer } from "./chat/chat-reducer";
 import ReduxThunk from "redux-thunk";
 
 const rootReducer = combineReducers({
-  session: sessionReducer,
   chat: chatReducer,
 });
 
