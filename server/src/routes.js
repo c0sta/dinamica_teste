49b4730ff495c3266c18257cd49d00eaa36fa696
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-08 13:37:36 -0300

    :art: Added routes to deal with user gens

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
-
+import UserController from "./controllers/UserController";
 const routes = Router();
 
-routes.get("/generate-users", (req, res) => {});
+routes.get("/users", UserController.listUsers);
+routes.get("/generate-users", UserController.generateUsers);
+routes.get("/delete-users", UserController.deleteUsers);
+
+export default routes;
