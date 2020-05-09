d415fed25aac112b14c322f25a86aa61637b8cda
Gabriel <gabrielcostamoura1@gmail.com>
2020-05-09 16:03:04 -0300

    :lipstick: Upgraded login page

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,6 +1,9 @@
 import React, { ReactElement } from "react";
 import { Link } from "react-router-dom";
-import logo from "../../assets/logo.jpeg";
+import { UserOutlined } from "@ant-design/icons";
+import image from "../../assets/img-01.png";
+// import image from "../../assets/logo.jpeg";
+
 import { Input, Button } from "antd";
 import "./Login.scss";
 
@@ -10,19 +13,22 @@ export function Login(): ReactElement {
   return (
     <div className="login">
       <form className="login__container">
-        <img className="container__logo" src={logo} alt="Company logo" />
+        <h1 className="container__title">Login</h1>
         <Input
           className="container__input"
           type="text"
           onChange={(e) => setName(e.target.value)}
+          size="large"
           placeholder="Username"
           required={true}
+          prefix={<UserOutlined className="input__icon" />}
         />
         <Button
           className="container__button"
           size="large"
           type="primary"
           htmlType="submit"
+          shape="round"
         >
           <Link to={`/app/user/${name || "userGuest"}`}>Sign In</Link>
         </Button>
@@ -34,6 +40,13 @@ export function Login(): ReactElement {
           Sign In as Consultant
         </Link>
       </form>
+      <section className="login__image">
+        <img
+          className="image__logo"
+          src={image}
+          alt="Notebook and lumninary'"
+        />
+      </section>
     </div>
   );
 }
